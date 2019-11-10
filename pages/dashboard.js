6a922a5c86c9feb6b3707e0acada3a8ab3030150
markoz <markdrew53@gmail.com>
2019-11-10 14:26:31 -0600

    Main view completed

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,28 +1,63 @@
+// Packages
 import React, { useState, useEffect } from 'react';
-import { Box, Badge, Image, Flex, Link, Text, Spinner } from '@chakra-ui/core';
+import { Box, Flex } from '@chakra-ui/core';
 
-import Layout from '../components/Layout';
+// Components
+import { Layout } from '../components/Layout';
+import { SpinnerLoad } from '../components/Spinner';
+import { Card } from '../components/Card';
+import { Button } from '../components/Button';
+import { MessageInfo } from '../components/MessageInfo';
+
+// Utils
+import { removeUser } from '../lib/utils/removeUser';
+import { createUser } from '../lib/utils/createUser';
 
 const Dashboard = () => {
-  const [data, setData] = useState([]);
+  const [users, setUser] = useState([]);
   const [isLoading, setLoading] = useState(null);
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
       const res = await fetch('https://anestesia.now.sh/api/user');
-      res.json().then(res => setData(res));
+      res.json().then(res => setUser(res));
       setLoading(false);
     };
     fetchData();
   }, []);
+  const approvedUser = (index, item) => {
+    removeUser('https://anestesia.now.sh/api/user', item._id);
+    createUser('https://anestesia.now.sh/api/approved', item);
+    const removeItem = [...users];
+    removeItem.splice(index, 1);
+    setUser(removeItem);
+  };
+  const rejectedUser = (index, item) => {
+    removeUser('https://anestesia.now.sh/api/user', item.id);
+    createUser('https://anestesia.now.sh/api/rejected', item);
+    const removeItem = [...users];
+    removeItem.splice(index, 1);
+    setUser(removeItem);
+  };
+  const checkUsers = () => {
+    if (users.length) {
+      return users.map((item, index) => (
+        <Card key={item._id} index={index} user={item}>
+          <Button action={() => approvedUser(index, item)} type="succes">
+            Aprobar
+          </Button>
+          <Button action={() => rejectedUser(index, item)} type="error">
+            Rechazar
+          </Button>
+        </Card>
+      ));
+    }
+    return <MessageInfo />;
+  };
   return (
     <Layout>
       <Flex align="center" justify="center" m={10}>
-        {isLoading ? (
-          <Spinner size="xl" color="#F49B0B" />
-        ) : (
-          <Box>usuarios penddientes</Box>
-        )}
+        {isLoading ? <SpinnerLoad /> : <Box>{checkUsers()}</Box>}
       </Flex>
     </Layout>
   );
