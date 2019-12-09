79d51d8282777bd940f061676f63f0f945dbf75c
markoz <markdrew53@gmail.com>
2019-12-09 12:49:07 -0600

    The dashboard page was added

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,19 +3,16 @@ import React, { useState, useEffect } from 'react';
 import { Box, Flex } from '@chakra-ui/core';
 
 // Components
-import { Layout } from '../components/Layout';
+import { Layout } from '../components/Dashboard/Layout';
 import { SpinnerLoad } from '../components/Spinner';
 import { Card } from '../components/Card';
 import { Button } from '../components/Button';
 import { MessageInfo } from '../components/MessageInfo';
 
-// Utils
-import { removeUser } from '../lib/utils/removeUser';
-import { createUser } from '../lib/utils/createUser';
-
 const Dashboard = () => {
   const [users, setUser] = useState([]);
   const [isLoading, setLoading] = useState(null);
+
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
@@ -25,28 +22,14 @@ const Dashboard = () => {
     };
     fetchData();
   }, []);
-  const approvedUser = (index, item) => {
-    removeUser('https://anestesia.now.sh/api/user', item._id);
-    createUser('https://anestesia.now.sh/api/approved', item);
-    const removeItem = [...users];
-    removeItem.splice(index, 1);
-    setUser(removeItem);
-  };
-  const rejectedUser = (index, item) => {
-    removeUser('https://anestesia.now.sh/api/user', item.id);
-    createUser('https://anestesia.now.sh/api/rejected', item);
-    const removeItem = [...users];
-    removeItem.splice(index, 1);
-    setUser(removeItem);
-  };
   const checkUsers = () => {
     if (users.length) {
       return users.map((item, index) => (
         <Card key={item._id} index={index} user={item}>
-          <Button action={() => approvedUser(index, item)} type="succes">
+          <Button action={() => {}} type="succes">
             Aprobar
           </Button>
-          <Button action={() => rejectedUser(index, item)} type="error">
+          <Button action={() => {}} type="error">
             Rechazar
           </Button>
         </Card>
@@ -55,12 +38,39 @@ const Dashboard = () => {
     return <MessageInfo />;
   };
   return (
-    <Layout>
+    <Layout tabIndex={0}>
       <Flex align="center" justify="center" m={10}>
         {isLoading ? <SpinnerLoad /> : <Box>{checkUsers()}</Box>}
       </Flex>
     </Layout>
   );
+};
+
+Dashboard.getInitialProps = ({ req, res }) => {
+  if (typeof window === 'undefined') {
+    // Packages
+    const Cookies = require('cookies');
+    const jwt = require('jsonwebtoken');
+
+    const cookie = new Cookies(req, res);
+    const token = cookie.get('token');
+    const dataDecoded = jwt.verify(
+      token,
+      '2BD41682-8DA5-438B-808A-05C4E12EC919'
+    );
+    const { verify } = dataDecoded;
+    if (!token) {
+      res.writeHead(301, { Location: '/login' });
+      res.end();
+      return {};
+    }
+    if (!verify) {
+      res.writeHead(301, { Location: '/login' });
+      res.end();
+      return {};
+    }
+    return { dataDecoded };
+  }
 };
 
 export default Dashboard;
