2d9ab787952affec67d880d7dac8422bb987e807
markoz <markdrew53@gmail.com>
2019-12-09 12:47:14 -0600

    Cookie settings

--- pages/index.js
+++ pages/index.js
@@ -6,4 +6,20 @@ const Index = () => {
   return <Dashboard />;
 };
 
+Index.getInitialProps = ({ req, res }) => {
+  if (typeof window === 'undefined') {
+    // Packages
+    const Cookies = require('cookies');
+
+    const cookie = new Cookies(req, res);
+    const token = cookie.get('token');
+    if (!token) {
+      res.writeHead(301, { Location: '/login' });
+      res.end();
+      return {};
+    }
+    return {};
+  }
+};
+
 export default Index;
