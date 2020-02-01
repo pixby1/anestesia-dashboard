1a9d466b004f6b3bc8d5acb274f0c296f92946fd
markoz <markdrew53@gmail.com>
2020-02-01 12:57:03 -0600

    Added search icon

---
+++ components/icons/search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Search = () => (
+  <svg
+    width="15"
+    height="15"
+    viewBox="0 0 13 13"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="#000"
+  >
+    <path d="M8.87 8.16l3.25 3.25-.7.71-3.26-3.25a5 5 0 1 1 .7-.7zM5 9a4 4 0 1 0 0-8 4 4 0 0 0 0 8z" />
+  </svg>
+);
+
+export default Search;
