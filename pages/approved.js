e1561c5632ef579302a233e7e548ab5a71dc3cb0
markoz <markdrew53@gmail.com>
2020-02-01 14:14:48 -0600

    Remove all logic card etc

--- pages/approved.js
+++ pages/approved.js
@@ -1,54 +1,13 @@
 // Packages
-import React, { useState, useEffect } from 'react';
-import { Flex, Box } from '@chakra-ui/core';
+import React from 'react';
 
 // Components
 import { Layout } from '../components/Dashboard/Layout';
-import { SpinnerLoad } from '../components/Spinner';
-import { Card } from '../components/Card';
-import { Button } from '../components/Button';
-import { MessageInfo } from '../components/MessageInfo';
 
 const Approved = () => {
-  const [users, setUser] = useState([]);
-  const [isLoading, setLoading] = useState(false);
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      const res = await fetch('/api/approved');
-      res.json().then(res => setUser(res));
-      setLoading(false);
-    };
-    fetchData();
-  }, []);
-  const handleClick = (index, item) => {
-    fetch('/api/removeUser', {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ id: item.id, userRemove: true })
-    }).then(() => {
-      const removeItem = [...users];
-      removeItem.splice(index, 1);
-      setUser(removeItem);
-    });
-  };
-  const checkUsers = () => {
-    if (users.length) {
-      return users.map((item, index) => (
-        <Card key={item._id} index={index} user={item}>
-          <Button type="error" action={() => handleClick(index, item)}>
-            Borrar usuario
-          </Button>
-        </Card>
-      ));
-    }
-    return <MessageInfo />;
-  };
   return (
     <Layout tabIndex={1}>
-      <Flex align="center" justify="center" m={10}>
-        {isLoading ? <SpinnerLoad /> : <Box>{checkUsers()}</Box>}
-      </Flex>
+      <h1>aprovado</h1>
     </Layout>
   );
 };
