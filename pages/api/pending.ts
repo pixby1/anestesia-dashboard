c5c8e93fa8c5531c2da78e7d52d48805ed5a286e
markoz <markdrew53@gmail.com>
2020-02-13 21:07:23 -0600

    Added progressive filter 🚀

--- pages/api/pending.ts
+++ pages/api/pending.ts
@@ -12,14 +12,19 @@ import { connectToDB } from '../../lib/db';
 
 async function hanlder(req: NextApiRequest, res: NextApiResponse) {
     await connectToDB(process.env.MONGODB_URI)
-    const { search } = req.query;
-    if (req.method === 'GET' && search) {
-        const user = await User.find({ name: search, state: 'PENDING' });
+    const { query } = req
+    if (req.method === 'GET' && Object.entries(query).length) {
+        const user = await User.find({ $and: [ { $or: [query] },  { state: 'APPROVED', userRemove: false } ] });
         return res.status(200).json(user);
     }
     if (req.method === 'GET') {
-        const users = await User.find({ state: 'PENDING' });
+        const users = await User.find({ state: 'PENDING', userRemove: false });
        return res.status(200).json(users);
+    }
+    if (req.method === 'PUT') {
+        const { id, state } = req.body;
+        await User.findByIdAndUpdate(id, { state });
+        return res.status(200).end();
     }
     return res.status(404).json({
         error: {
